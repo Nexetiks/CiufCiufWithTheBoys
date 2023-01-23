112b4461ba5e13c7005281cbd50216face64eac9
Pawelek <adam.pawelko01@gmail.com>
2023-01-23 14:08:15 +0100

    Work form last week i forgot to commit.

--- Assets/RD/Pawelek/Scripts/MoveUnitAbility.cs
+++ Assets/RD/Pawelek/Scripts/MoveUnitAbility.cs
@@ -8,6 +8,7 @@ namespace Pawelek.Testing.ItWillBeChangeLater
     public class MoveUnitAbility : Ability<MoveUnitAbilityArgs>
     {
         private MoveUnitTriggerEffect moveUnitTriggerEffect;
+        private MoveUnitContinuousEffect moveUnitContinuousEffect;
         protected override TriggeredEffect<MoveUnitAbilityArgs> DefaultTriggeredEffect => moveUnitTriggerEffect;
         protected override ContinuousEffect<MoveUnitAbilityArgs> DefaultEffectInUpdate { get; }
         protected override ContinuousEffect<MoveUnitAbilityArgs> DefaultEffectInFixedUpdate { get; }
@@ -15,13 +16,14 @@ namespace Pawelek.Testing.ItWillBeChangeLater
         public MoveUnitAbility(string name) : base("Move Unit Ability")
         {
             moveUnitTriggerEffect = new MoveUnitTriggerEffect();
+            moveUnitContinuousEffect = new MoveUnitContinuousEffect();
         }
 
         public override void Initialize(Entity abilityOwner)
         {
             base.Initialize(abilityOwner);
 
-            Args = new MoveUnitAbilityArgs(abilityOwner, abilityOwner.GameObject.GetComponent<Rigidbody2D>(), abilityOwner.GameObject.transform);
+            Args = new MoveUnitAbilityArgs(abilityOwner, abilityOwner.GameObject.GetComponent<Rigidbody2D>(), abilityOwner.GameObject.transform, Vector2.zero, 100f, default); // TODO change
         }
     }
 }
